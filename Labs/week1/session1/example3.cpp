8a00e9cdd0403bbf30a9e7b39b9383f98f2cabad
thomas <didymusthomas6@gmail.com>
2026-01-22 12:05:57 +0300

    Update example3 for cleanup

--- Labs/week1/session1/example3.cpp
+++ Labs/week1/session1/example3.cpp
@@ -13,27 +13,3 @@ int main(){
   cout<< a + b;
 }
 
-
-#include <iostream>
-using namespace std;
-
-int main(){
-  int a,b;
-  for( int i=0; i<3; i++){
-      cout<<"Enter the first number" <<endl;
-      cin>>a;
-      cout<<"Enter the sec number" <<endl;
-      cin>>b;
-
-      if (a>b){
-          cout<<a;
-      } else if (b>a){
-          cout<<b;
-      } else {
-          cout<<"They are equal";
-      }
-  }
-
-  return 0;
-
-}
