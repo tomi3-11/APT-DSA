5b0a6d580fe276653bea9c2a26982c4d4aa91242
thomas <didymusthomas6@gmail.com>
2026-01-22 11:35:12 +0300

    Add day 1 example 1 content

---
+++ Labs/week1/session1/example1.cpp
@@ -0,0 +1,16 @@
+// example 1: This focuses on understanding and output in c++
+// Date: Tuesday 13th/January/2026
+
+
+// This is necessary in c++ to make the 'cout' functions operational.
+#include <iostream>
+using namespace std;
+
+// This is the main function that handles all the operations of this program; like an entry point
+int main(){
+    // This displays a simple message to the terminal; equivalent of the 'print' in some languages
+    cout << "Hello, World!";
+    return 0;
+}
+
+
